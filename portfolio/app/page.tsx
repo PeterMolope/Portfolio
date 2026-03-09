ef490a124f112bf8cf6b768921e177037da6b19e
Peter Molope <ptrmolope@gmail.com>
2026-03-09 12:32:26 +0200

    layout setup

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
-  return <main>Homepage</main>;
+  return (
+    <div className="p-6">
+      {/* TAILWIND test */}
+      <p className="text-5xl text-red-500">PORTFOLIO 2026</p>
+      {/* SHADCN test */}
+      <Button variant="secondary" size={"lg"}>SHADCN BUTTON</Button>
+    </div>
+  );
   
 }
