dd3b24c3e71483a4c1e22880061bcdea9a9a487d
Peter Molope <ptrmolope@gmail.com>
2026-03-10 10:56:17 +0200

    refactor: clean up code by removing unused AboutPage and TeamPage components, and streamline Home component return statements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
 import type { Metadata } from "next";
-import { Inter,JetBrains_Mono } from "next/font/google";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 const jetBrainsMono = JetBrains_Mono({
-  weight: ["100","200","300","400", "500", "600", "700","800"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
   variable: "--font-jetBrainsMono",
   subsets: ["latin"],
 });
 
 const inter = Inter({
-    weight: ["100","200","300","400", "500", "600", "700","800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "Peter Molope Portfolio",
-  description: " This project leverages the latest features of the Next.js ecosystem to provide a lightning-fast, SEO-optimized platform. By utilizing the App Router, React Server Components, and advanced caching strategies, the application ensures minimal client-side JavaScript while maintaining a highly interactive UI.",
+  description:
+    " This project leverages the latest features of the Next.js ecosystem to provide a lightning-fast, SEO-optimized platform. By utilizing the App Router, React Server Components, and advanced caching strategies, the application ensures minimal client-side JavaScript while maintaining a highly interactive UI.",
 };
 
 export default function RootLayout({
@@ -28,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
+      <body className={`${inter.className} antialiased`}>
         Navbar
         {children}
         Footer
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,5 @@ import Home from "@/components/Home/Home";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
-  return (
-    <Home />
-  );
-  
+  return <Home />;
 }
