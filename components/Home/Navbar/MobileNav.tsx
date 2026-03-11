12aad5bf9728e68ea69eb1810b72fcdb69f9c065
Peter Molope <ptrmolope@gmail.com>
2026-03-11 11:25:33 +0200

    feat: improve MobileNav component with enhanced overlay and layout adjustments for better user experience

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -7,34 +7,42 @@ type MobileNavProps = {
   isMobileNavOpen: boolean;
   closeMobileNav: () => void;
 };
+
 const MobileNav = ({ isMobileNavOpen, closeMobileNav }: MobileNavProps) => {
+  // Slide from the left (-100% to 0)
   const mobileNavClasses = isMobileNavOpen
     ? "translate-x-0"
-    : "translate-x-[100%]";
+    : "-translate-x-full";
+
   return (
     <div>
-      {/* Overlay */}
+      {/* Overlay: We use opacity and pointer-events instead of translate here */}
       <div
-        className={`fixed ${mobileNavClasses} inset-0 transform transition-all duration-500 z-1002 bg-black opacity-70 w-full h-screen `}
+        onClick={closeMobileNav}
+        className={`fixed inset-0 z-[10002] bg-black transition-all duration-500 ${
+          isMobileNavOpen ? "opacity-70 visible" : "opacity-0 invisible"
+        }`}
       ></div>
 
-      {/* Updated container with increased vertical spacing */}
+      {/* Nav Container */}
       <div
-        className={`text-white ${mobileNavClasses} fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-gradient-to-br from-blue-900 to-black rounded-r-3xl z-1050 space-y-10`}
+        className={`fixed left-0 top-0 h-full w-[80%] sm:w-[60%] z-[10005] 
+        bg-gradient-to-br from-blue-900 to-black rounded-r-3xl 
+        flex flex-col justify-center space-y-10 text-white 
+        transition-all duration-500 transform ${mobileNavClasses}`}
       >
-        {Navlinks.map((link, index) => {
-          return (
-            <Link key={index} href={link.href}>
-              <p className="text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white hover:text-blue-300 transition-colors duration-200">
-                {link.name}
-              </p>
-            </Link>
-          );
-        })}
-        {/**close icon */}
+        {Navlinks.map((link, index) => (
+          <Link key={index} href={link.href} onClick={closeMobileNav}>
+            <p className="text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white hover:text-blue-300 transition-colors duration-200">
+              {link.name}
+            </p>
+          </Link>
+        ))}
+
+        {/* Close Icon - Positioned absolute to the container */}
         <X
           onClick={closeMobileNav}
-          className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6"
+          className="absolute top-[1.5rem] right-[1.5rem] sm:w-8 sm:h-8 w-6 h-6 cursor-pointer hover:scale-110 transition-transform"
         />
       </div>
     </div>
